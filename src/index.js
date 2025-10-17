f899a2f377cdd6bddee486d3e82ec7fff8299664
DebraChinhengo <dchinhengo@gmail.com>
2025-10-18 07:09:13 +1000

    Added form handling and typewritter JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateTsumo(event) {
+  event.preventDefault();
 
+  let tsumoElement = document.querySelector("#tsumo");
+  new Typewriter("#tsumo", {
+    strings: ["Moyo Muti Unomera Paunoda"],
+    autoStart: true,
+    delay: 75,
+    cursor: "|",
+  });
+}
+
+let tsumoFormElement = document.querySelector("#tsumoGeneratorForm");
+tsumoFormElement.onsubmit = generateTsumo;
